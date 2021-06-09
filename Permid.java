8c7f0e05af818afdd82946e2eee6a31b0cceaf11
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-09 20:45:56 +0530

    Add files via upload

---
+++ Permid.java
@@ -0,0 +1,18 @@
+class Permid
+{
+public static void main(String[] args)
+{
+for(int i=1;i<=5;i++)
+{
+  for(int j = 5; j>i;j--)
+{
+  System.out.print(" ");
+}
+    for(int n=1;n<=i;n++)
+{
+      System.out.print("* "); 
+}
+          System.out.println();
+}
+}
+} 
